2b8cc94c456ebe93ab1d4387e0c22d7a8d25ea26
Matt Latham <latham75@gmail.com>
2016-10-06 17:24:25 -0500

    right/wrong guess to dom

--- public/main.js
+++ public/main.js
@@ -5,9 +5,9 @@ const socket = io()
 //----------MOVE TO BACKEND----------------
 
 const wordSelect = () => {
-const spookyWords = [
-  "afraid", "apparition", "bloodcurdling", "bloody", "bonechilling", "bones", "broomstick", "cackle", "cadaver", "carve", "casket", "cauldron", "cemetery", "chilling", "cobweb", "coffin", "costume", "crawly", "creature", "creepy", "dark", "decapitate", "dew", "disembowel", "dreadful", "exsanguinate", "fangtastic", "frightening", "ghostly", "ghoulish", "goblin", "gory", "grave", "gruesome", "haunted", "hellhound", "howl", "lovecraftian", "macabre", "mausoleum", "moonlit", "morbid", "mummy", "ominous", "party", "phantom", "poltergeist", "potion", "pumpkin", "scary", "scott", "scream", "shadow", "skeleton", "skull", "socketio", "specter", "spell", "spider", "spirits", "spooky", "supernatural", "superstition", "terrifying", "tests", "tombstone", "treat", "trick", "undead", "unearthly", "unnerving", "vampire", "warlock", "werewolf", "witch", "wizard", "wraith", "zombie"
-]
+  const spookyWords = [
+    "afraid", "apparition", "bloodcurdling", "bloody", "bonechilling", "bones", "broomstick", "cackle", "cadaver", "carve", "casket", "cauldron", "cemetery", "chilling", "cobweb", "coffin", "costume", "crawly", "creature", "creepy", "dark", "decapitate", "dew", "disembowel", "dreadful", "exsanguinate", "fangtastic", "frightening", "ghostly", "ghoulish", "goblin", "gory", "grave", "gruesome", "haunted", "hellhound", "howl", "lovecraftian", "macabre", "mausoleum", "moonlit", "morbid", "mummy", "ominous", "party", "phantom", "poltergeist", "potion", "pumpkin", "scary", "scott", "scream", "shadow", "skeleton", "skull", "socketio", "specter", "spell", "spider", "spirits", "spooky", "supernatural", "superstition", "terrifying", "tests", "tombstone", "treat", "trick", "undead", "unearthly", "unnerving", "vampire", "warlock", "werewolf", "witch", "wizard", "wraith", "zombie"
+  ]
   const random = Math.floor(Math.random() * (spookyWords.length - 1)) + 1
   const selectedWord = spookyWords[random]
   console.log("word", selectedWord)
@@ -15,7 +15,7 @@ const spookyWords = [
 }
 wordSelect()
 let splitArray = []
-// Converting word to array of letter strings
+  // Converting word to array of letter strings
 const wordConvert = (selectedWord) => {
   splitArray = selectedWord.split('')
   console.log("split", splitArray)
@@ -23,8 +23,9 @@ const wordConvert = (selectedWord) => {
 }
 
 // Converting string letters to underscores
+let scoredArray = []
 const underScore = (selectedWord) => {
-  const scoredArray = selectedWord.replace(/./g, '__.').split('.')
+  scoredArray = selectedWord.replace(/./g, '__.').split('.')
   scoredArray.pop()
   console.log(typeof scoredArray)
   console.log("scoredArray", scoredArray);
@@ -35,42 +36,56 @@ const underScore = (selectedWord) => {
 
 // Putting underscores to DOM with spacing
 const arrayOutput = (scoredArray) => {
-	const space = " "
-	const outPut = ""
-	for(var score in scoredArray) {
-		$('.theWord').append(scoredArray[score])
-		$('.theWord').append(space)
-	}
+  const space = " "
+  $('.theWord').text('')
+  for (var score in scoredArray) {
+    $('.theWord').append(scoredArray[score])
+    $('.theWord').append(space)
+  }
 }
 
-  // $('.theWord').html(scoredArray)
 underScore('werewolf')
 
 wordConvert("werewolf")
 
 $('#spin').on('click', () => {
-	console.log("urmom");
+  console.log("urmom");
 })
 
 $('#enterLetter').on('click', () => {
-	let letter = $('#letter').val().toLowerCase()
-	console.log("letter", letter);
-	letterCheck(letter)
-	// Regex for value being only one letter
-	// function to add or reject letter
-	// socket emit result
+  //need Regex check here
+  let letter = $('#letter').val().toLowerCase()
+  console.log("letter", letter);
+  letterCheck(letter)
 })
 
 // Return the array position of the guessed letter
+let indices = []
 const letterCheck = (letter) => {
-	let indices = []
-	let result = splitArray.filter((x, index) => {
-		if (x === letter) {
-			indices.push(index)
-		}
-		return indices
-	})
+    let result = splitArray.filter((x, index) => {
+      if (x === letter) {
+        indices.push(index)
+      }
+    })
+    console.log("index", indices)
+    arraySwitch(letter)
+  }
+
+const arraySwitch = (letter) => {
+  console.log("scored", scoredArray)
+  if (indices.length === 0) {
+    $('#missLetters').append(letter)
+  } else {
+    for (var i = 0; i < indices.length; i++) {
+      console.log("in loop", indices[i], letter)
+      scoredArray.splice(indices[i], 1, letter)
+    }
+    console.log("new", scoredArray)
+    indices = []
+    arrayOutput(scoredArray)
+  }
 }
+
 
 $('#guessWord').on('click', () => {
 
